bcb4138d012086ee81f7baa4428f6294a33deb8d
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-12-02 18:19:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+bool primo(int numero, int i) {
+  int resto=numero%i;
+  
+  if(numero<=i)return true;
+  if(resto==0&&i>1)return false;
+  if(resto!=0) {
+    i++;
+    primo(numero, i);
+  }
+  else return true;//resto!=0&&i<=1
