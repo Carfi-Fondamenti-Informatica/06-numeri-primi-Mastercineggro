04aa5b8318beb9d101e254faba091ee646ef5d68
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-12-02 18:08:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
+int primo(int,int);
 int main(){
+  int n,i=2;
+  cin>>n;
+  if(primo(n,i)) cout<<"numero primo";
+  else cout<<"numero non primo";
   return 0;
 }
